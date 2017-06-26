c08f09a031ede5afdf86f1b973bc1f63ed294bea
Serhii Panchenko <serhii_panchenko@epam.com>
2017-06-26 15:51:12 +0300

    PING_STAT.sh was added

---
+++ BASH/PING_STAT.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+HOST=$1
+SCRIPT_NAME=$0
+touch log.txt
+if [[ -z ${HOST} ]];
+  then
+    echo "Syntax error."
+    echo "==============================================================="
+    echo "${SCRIPT_NAME} HOST_ADRESS "
+    echo "==============================================================="
+    exit 1
+else
+  echo "Pinging ${HOST}, please wait..."
+  for i in range {5000..65000..5000}
+    do
+    ping -w 3 -c 100 -s ${i} -l 100 -q ${HOST} > log.txt 2>/dev/null
+    echo "For packet size ${i} Loss is $(grep -e "[[:digit:]]*% packet loss" log.txt | cut -d ' ' -f6)"
+    done
+fi
+cat log.txt
+exit 0
