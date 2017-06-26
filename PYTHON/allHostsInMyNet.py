dd73705e3d2038af56522587bb5a3eaa3832e164
Serhii Panchenko <serhii_panchenko@epam.com>
2017-06-26 18:02:02 +0300

    allHostsInMyNet.py added

--- PYTHON/allHostsInMyNet.py
+++ PYTHON/allHostsInMyNet.py
@@ -2,7 +2,7 @@ from netaddr import *
 import os
 
 for ip in IPNetwork('10.23.12.204/20').iter_hosts():
-    response = os.system("ping -t -n 1 -w 10 %s" % ip)
+    response = os.system("ping -t -n 1 -w 10 %s" % ip )
     if response == 0:
         print ip, 'is up!'
     else:
