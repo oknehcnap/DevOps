fb0fd7a6dd12518e9434691132a990ec8b67b73c
Serhii Panchenko <serhii_panchenko@epam.com>
2017-06-29 18:49:38 +0300

    fixed bugs

--- BASH/PING_STAT.sh
+++ BASH/PING_STAT.sh
@@ -12,9 +12,9 @@ if [[ -z ${HOST} ]];
     exit 1
 else
   echo "Pinging ${HOST}, please wait..."
-  for i in range {5000..65000..5000}
+  for i in range {100..1000..100}
     do
-    ping -w 3 -c 100 -s ${i} -l 100 -q ${HOST} > log.txt 2>/dev/null
+    ping -f -l ${i} -s 1024 -q ${HOST} > log.txt 2>/dev/null
     echo "For packet size ${i} Loss is $(grep -e "[[:digit:]]*% packet loss" log.txt | cut -d ' ' -f6)"
     done
 fi
