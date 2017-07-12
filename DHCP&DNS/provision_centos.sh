9e90916414c25513a0f71bdba5061c05d49a51a6
Serhii Panchenko <serhii_panchenko@epam.com>
2017-07-12 19:27:30 +0300

    DNS DHCP

--- DHCP&DNS/provision_centos.sh
+++ DHCP&DNS/provision_centos.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 su
 yum update -y
+
 #DHCP
 yum install dhcp -y
 cat /vagrant/provision/centos_dhcpd.conf > /etc/dhcp/dhcpd.conf
 cat /vagrant/provision/centos_dhcpd > /etc/sysconfig/dhcpd
-systemctl restart dhcpd
-systemctl enable dhcpd
 #DNS
 yum install bind bind-utils -y
-cat /vagrant/provision/centos_named.conf > /etc/named.conf
-cat /vagrant/provision/forward.serhiipanchenko > /var/named/forward.serhiipanchenko
-cat /vagrant/provision/reverse.serhiipanchenko > /var/named/reverse.serhiipanchenko
-systemctl enable named
-systemctl start named
-firewall-cmd --permanent --add-port=53/tcp
-firewall-cmd --permanent --add-port=53/udp
-firewall-cmd --reload
-chgrp named -R /var/named
-chown -v root:named /etc/named.conf
+cat /vagrant/provision/dns/centos_named.conf > /etc/named.conf
+cat /vagrant/provision/dns/forward.serhiipanchenko > /var/named/data/forward.serhiipanchenko
+cat /vagrant/provision/dns/reverse.serhiipanchenko > /var/named/data/reverse.serhiipanchenko
+cat /vagrant/provision/dns/serhiiDevOps.org > /var/named/data/serhiiDevOps.org
+chmod 755 -R /var/named
+chown -v -R named:named /var/named
+chown -v named:named /etc/named.conf
 restorecon -rv /var/named
 restorecon /etc/named.conf
-cat /vagrant/provision/ifcfg-eth1 > /etc/sysconfig/network-scripts/ifcfg-eth1
-
+cat /vagrant/provision/dns/ifcfg-eth1 > /etc/sysconfig/network-scripts/ifcfg-eth1
 systemctl restart network
+systemctl restart named
+cat /etc/rndc.key > /vagrant/dhcp2_key.conf
+systemctl restart dhcpd
+systemctl restart named
+printf "search serniipanchenko.local\nnameserver 172.16.60.1\n" > /etc/resolv.conf 
--- DHCP&DNS/provision_ubuntu.sh
+++ DHCP&DNS/provision_ubuntu.sh
@@ -2,7 +2,11 @@
 
 sudo su
 apt-get update
+#DHCP
 apt-get install isc-dhcp-server -y
 cat /vagrant/provision/ubuntu_isc-dhcp-server > /etc/default/isc-dhcp-server
-cat /vagrant/provision/ubuntu_dhcpd.conf >  /etc/dhcp/dhcpd.conf
+cat /vagrant/provision/ubuntu_dhcpd.conf > /etc/dhcp/dhcpd.conf
+cat /vagrant/dhcp2_key.conf > /etc/dhcp/dhcp2_key.conf && echo "OK"
+chown dhcpd:dhcpd /etc/dhcp/dhcp2_key.conf
+printf "search serhiiDevOps.org.\nnameserver 172.16.60.1\n" > /etc/resolv.conf 
 service isc-dhcp-server restart
