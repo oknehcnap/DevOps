ba65a3bbf3f83ab7c96476c457df9375e404eb3c
Serhii Panchenko <serhii_panchenko@epam.com>
2017-08-10 19:45:54 +0300

    jenkins scr has been added

--- JENKINS/RBPS.groovy
+++ JENKINS/RBPS.groovy
@@ -1,28 +1,22 @@
-//Скрипт для добавления списка людей в Role-Based Strategy plugin.
+@Grab('org.yaml:snakeyaml:1.17')
 
+import org.yaml.snakeyaml.Yaml
 import jenkins.model.*
 import hudson.security.*
 import java.util.*
 import com.michelin.cio.hudson.plugins.rolestrategy.*
 import java.lang.reflect.*
-def roleName = "chief" //Имя роли
-def roleType = RoleBasedAuthorizationStrategy.GLOBAL // Возможные роли .GLOBAL .PROJECT .SLAVE
-def userName = [		//Список пользователей
-	"test@test.com",
-	"test1@test.com",
-	"test2@test.com",
-	"test3@test.com",
-	"test4@test.com",
-	"test5@test.com", 
-	"test6@test.com",
-	"test7@test.com",
-	"test8@test.com",
-	"test9@test.com",
-	"test10@test.com",
-  ]
 
-def findGuestRoleEntry(grantedRoles, roleName) //Проверка существования роли.
-{
+def roleType = RoleBasedAuthorizationStrategy.PROJECT
+Yaml parser = new Yaml()
+def doc = parser.load(("/var/lib/jenkins/roles.yml" as File).text)
+def userMap = [:]
+
+doc.each{
+ 	userMap.put("${it.key}","${it.value}")	
+}
+
+def findGuestRoleEntry(grantedRoles, roleName) {
   for (def entry : grantedRoles)
   {
     Role role = entry.getKey()
@@ -36,35 +30,10 @@ def findGuestRoleEntry(grantedRoles, roleName) //Проверка существ
 }
 
 def authStrategy = Jenkins.getInstance().getAuthorizationStrategy()
+RoleBasedAuthorizationStrategy roleAuthStrategy = (RoleBasedAuthorizationStrategy) authStrategy
+def grantedRoles = authStrategy.getGrantedRoles(roleType)
 
-if(authStrategy instanceof RoleBasedAuthorizationStrategy){
-  RoleBasedAuthorizationStrategy roleAuthStrategy = (RoleBasedAuthorizationStrategy) authStrategy
-  def grantedRoles = authStrategy.getGrantedRoles(roleType) //Объект типа map ролей для roleType
-  if (grantedRoles != null)
-  {
-
-    def roleEntry = findGuestRoleEntry(grantedRoles, roleName) //Нахождение записи существующей роли roleName.
-    if (roleEntry != null)
-    {
-
-      def sidList = roleEntry.getValue()
-      for (String user : userName){
-        if (sidList.contains(user)) //Проверка того, что пользователю уже присвоена роль
-        {
-          println "User " + user + " already assigned to role " + roleName
-        } else {
-          println "Adding user " + user + " to role " + roleName
-         roleAuthStrategy.assignRole(roleType, roleEntry.getKey(), user); //Присвоение роли
-          println "OK"
-        }
-      }
-      Jenkins.instance.save()
-    } else {
-      println "Unable to find role " + roleName
-    }
-  } else {
-    println "Unable to find grantedRoles for " + roleType
-  }
-} else {
-  println "Role Strategy Plugin not found!"
+for ( e in userMap ) {
+   	def roleEntry = findGuestRoleEntry(grantedRoles, "$e.value".toString())
+	roleAuthStrategy.assignRole(roleType, roleEntry.getKey(), "$e.key".toString());
 }
